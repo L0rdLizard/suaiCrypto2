be702e8ab11b73a9fb0c0e3045ad3b9931ede47c
L0rdLizard <WarriorXKZ@gmail.com>
2024-01-03 16:19:30 +0300

    Add .gitignore and CMakeLists.txt for lab04

---
+++ lab04/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a;
+
+    //scanf_s("%d", a);
+
+    printf("vvedi chislo pipiska\n");
+    scanf_s("%d", &a);
+    
+    cout << "hellou chat";
+
+    return 0;
+}
